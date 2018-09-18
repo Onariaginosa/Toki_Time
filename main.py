5ff40572d7e015c91a41ea3d35892ae1bd163663
Ameya Mellacheruvu <ameyacapps@gmail.com>
2018-09-17 19:44:57 -0700

    main.py changes

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ jinja_env = jinja2.Environment(
 
    class MainPage(webapp2.RequestHandler):
       def get(self):
-        welcome_template = jinja_env.get_template("templates/welcome_template.html")
+        welcome_template = jinja_env.get_template("templates/welcome.html")
         self.response.write(welcome_template.render())
 
-    app = webapp2.WSGIApplication([
-        ('/', MainPage),
-    ], debug=True)
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
